c5579a6300d8b26255228e632c0b3072e1a957c6
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2020-09-19 00:46:10 -0400

    Added getOrg controller method

--- src/controllers/org.js
+++ src/controllers/org.js
@@ -36,10 +36,11 @@ exports.addOrg = async (req, res) => {
   } catch (error) {
     res
       .status(404)
-      .json({ error: 'Threre was problem adding this organization' });
+      .json({ error: 'There was problem adding this organization' });
   }
+};
 
-  /**
+/**
  * @function updateOrg
  * @description Updates organization in database
  * @param {*} query orgId
@@ -47,38 +48,57 @@ exports.addOrg = async (req, res) => {
  * @param {*} res confirmation msg
  */
 
-  exports.updateOrg = async (req, res) => {
-    const orgId = req.query;
-    const {
-      name,
-      addressLine1,
-      addressLine2,
-      city,
-      state,
-      zipCode,
-      photo,
-      active
-    }
-    try {
-      const org = await Org.findOne({ _id: orgId });
-      if (!org)
-        return res.status(400).json({ error: 'Organization not found' });
-      
-      if (name) org.name = name;
-      if (addressLine1) org.addressLine1 = addressLine1;
-      if (addressLine2) org.addressLine2 = addressLine2;
-      if (city) org.city = city;
-      if (state) org.state = state;
-      if (zipCode) org.zipCode = zipCode;
-      if (photo) org.photo = photo;
-      if (active) org.active = active;
+exports.updateOrg = async (req, res) => {
+  const { orgId } = req.query;
+  const {
+    name,
+    addressLine1,
+    addressLine2,
+    city,
+    state,
+    zipCode,
+    photo,
+    active,
+  } = req.body;
 
-      await org.save();
-      res.send({msg: `${org.name} has been successfully updated`})
-    } catch (error) {
-      res
+  try {
+    const org = await Org.findOne({ _id: orgId });
+    if (!org) return res.status(400).json({ error: 'Organization not found' });
+
+    if (name) org.name = name;
+    if (addressLine1) org.addressLine1 = addressLine1;
+    if (addressLine2) org.addressLine2 = addressLine2;
+    if (city) org.city = city;
+    if (state) org.state = state;
+    if (zipCode) org.zipCode = zipCode;
+    if (photo) org.photo = photo;
+    if (active) org.active = active;
+
+    await org.save();
+    res.send({ msg: `${org.name} has been successfully updated` });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error: 'There was a problem updating this organization' });
+  }
+};
+
+/**
+ * @function getOrgs
+ * @description List all organizations in database
+ * @param {*} res List of all orgs
+ */
+
+exports.getOrgs = async (req, res) => {
+  try {
+    const orgs = await Org.find({});
+    if (!orgs)
+      return res
         .status(400)
-        .json({ error: 'There was a problem updating this organization' });
-    }
-  };
+        .json({ error: "There's an error with getting the organizations" });
+
+    res.send(orgs);
+  } catch (error) {
+    res.status(400).json({ error: "There's a problem getting organizations" });
+  }
 };
--- src/routes/org.js
+++ src/routes/org.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { addOrg, updateOrg } = require('../controllers/org');
+const { addOrg, updateOrg, getOrgs } = require('../controllers/org');
 const router = express.Router();
 
 router.post('/add', addOrg);
 
 router.put('/update', updateOrg);
 
+router.get('/', getOrgs);
 module.exports = router;
