c5c0f0716b41b90592bb461020150bbc1844870f
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2020-09-21 19:14:28 -0400

    Added forgotPasswordVerification to auth controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -197,6 +197,124 @@ exports.emailVerificationCheck = async (req, res) => {
       payload,
       process.env.JWT_SECRET,
       { expiresIn: '2h' },
+      async (err, token) => {
+        if (err) return res.status(400).json({ error: err.message });
+        userSession.token = token;
+        await userSession.save();
+        res.send({
+          msg: 'Your email has been successfully verified.',
+          token,
+          id: decoded.id,
+          role: decoded.role,
+          org: decoded.org,
+        });
+      },
+    );
+  } catch (error) {
+    res
+      .status(401)
+      .json({ error: 'There was a problem verifying this email.' });
+  }
+};
+
+/**
+ * @function forgotPassword
+ * @description Sends user email for forgotPasswordVerification
+ * @param {*} req body email
+ * @param {*} res confirmation msg
+ */
+
+exports.forgotPassword = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await User.find({ email });
+    if (!user)
+      return res
+        .status(400)
+        .json({ error: 'There was a problem finding your account' });
+
+    const userSession = Session.find({ user: user._id });
+    if (!userSession)
+      return res
+        .status(400)
+        .json({ error: 'There was a problem finding your account' });
+
+    const payload = {
+      id: user._id,
+      role: user.role,
+      org: user.org,
+      tokenType: tokenTypes.PASSWORD_CHANGE_VERIFICATION,
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: '2h' },
+      async (err, token) => {
+        if (err) return res.status(400).json({ error: err.message });
+
+        userSession.token = token;
+        await userSession.save();
+
+        const link = `${process.env.FRONTEND_URL}/registration-email-verification?token=${token}`;
+
+        // Send user confirmation/verification email with token link
+        sendMail({
+          from: 'contact@grandmaemmas.com',
+          to: email,
+          subject: "No-Reply - Grandma Emma's - Forgot Password Verification",
+          text: "Grandma Emma's verify that you forgot your password",
+          html: `<p>Hi ${firstName}</p>
+             <p>This is an email verifying that you forgot your password.</p>
+             <p>Please click the link below to verify your email.</p>
+             <br></br>
+             <p>${link}
+             <br></br>
+             <p>Thank you,</p>
+             <p>Grandma Emmas Team`,
+        });
+      },
+    );
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+/**
+ * @function forgotPasswordVerification
+ * @description Gets token from email to check if user has password verification token in db
+ * @param {*} query token
+ * @param {*} req
+ * @param {*} res confirmation msg
+ */
+
+exports.forgotPasswordVerification = async (req, res) => {
+  const { token } = req.query;
+  try {
+    const userSession = Session.find({ token });
+    if (!userSession)
+      return res
+        .status(401)
+        .json({ error: 'There was a problem verifying this email.' });
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded)
+      return res
+        .status(401)
+        .json({ error: 'Your verification session has expired.' });
+
+    const payload = {
+      id: decoded.id,
+      role: decoded.role,
+      org: decoded.org,
+      tokenType: tokenTypes.SESSION,
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: '2h' },
       (err, token) => {
         if (err) return res.status(400).json({ error: err.message });
         userSession.token = token;
@@ -216,6 +334,13 @@ exports.emailVerificationCheck = async (req, res) => {
   }
 };
 
+/**
+ * @function tokenVerify
+ * @description Verifies token from frontend and compares to session token in db
+ * @param {*} query token
+ * @param {*} req header
+ */
+
 exports.tokenVerify = (req, res, next) => {
   // Get token from header
   const token = req.header('x-auth-token');
@@ -228,7 +353,9 @@ exports.tokenVerify = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user;
+    res.locals.ID = decoded.id;
+    res.locals.ROLE = decoded.role;
+    res.locals.ORG = decoded.org;
     next();
   } catch (err) {
     res
